a74a1cf9fb043508c1f79d966babd4795b637fdb
lazzyoung <sprvk0325@naver.com>
2025-04-01 12:55:01 +0900

    feat: 서버,DB 개설 서버 개설하였고 추가될 route에서 DB이용 가능하도록 별도로 분리하였음.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const axios = require('axios'); 
+const jwt = require('jsonwebtoken'); 
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT
+
+let connectDB = require('./database.js')
+let db
+connectDB.then((client)=>{
+  db = client.db('triangle')
+
+  app.listen(port, () => {
+    console.log('서버연결성공')
+})
+
+}).catch((err)=>{
+  console.log(err)
+})
+
+app.use(express.json());
+
